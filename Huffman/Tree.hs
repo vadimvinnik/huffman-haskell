7f68d9db7ee2edd70e008273acda03e02042f1a8
vadim.vinnik <vadim.vinnik@gmail.com>
2014-08-11 22:35:49 +0000

    Add an encoder

--- Huffman/Tree.hs
+++ Huffman/Tree.hs
@@ -1,10 +1,27 @@
 module Huffman.Tree (
-  itemsToCodes 
+  bytesToBitCodes
 ) where
 
 import Huffman.Tree.Impl
+import qualified Data.Word
+import qualified Data.Binary.BitBuilder as B
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as C
 import qualified Data.Map as M
 
-itemsToCodes :: Ord a => b -> (b -> b) -> (b -> b) -> [a] -> [a] -> M.Map a b
-itemsToCodes s f g v = (freqTreeToCodes s f g) . (itemsToFreqTree v)
+type Byte = Data.Word.Word8
 
+bytesToBitCodes :: [Byte] -> M.Map Byte B.BitBuilder
+bytesToBitCodes = itemsToCodes B.empty (appendBit False) (appendBit True) [minBound::Byte .. maxBound::Byte] where
+  appendBit b = B.append $ B.singleton b
+
+huffmanEncodeWith :: M.Map Byte B.BitBuilder -> [Byte] -> L.ByteString
+huffmanEncodeWith m t = B.toLazyByteString $ foldl encodeByte B.empty t where
+  encodeByte s b = B.append (m M.! b) s
+
+huffmanEncode :: [Byte] -> L.ByteString
+huffmanEncode s = huffmanEncodeWith (bytesToBitCodes s) s
+
+huffmanEncodeString :: String -> L.ByteString
+huffmanEncodeString = huffmanEncode . S.unpack . C.pack
