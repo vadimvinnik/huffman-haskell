89e54dda133e6b7c7860446ccd7133ad9b296a4e
vadim.vinnik@gmail.com <vadim.vinnik@gmail.com@84efa2fc-7ed4-2164-f6bb-8c9606cd4d4f>
2014-08-13 11:28:00 +0000

    Delete a redundant function

--- trunk/Huffman/Tree/Impl.hs
+++ trunk/Huffman/Tree/Impl.hs
@@ -4,7 +4,6 @@
 
 module Huffman.Tree.Impl (
   itemsToFreqTable,
-  freqTableToForest,
   freqForestToTree,
   itemsToFreqTree,
   freqTreeToCodes,
@@ -29,7 +28,7 @@ itemsToFreqTable v t = M.fromListWith (+) ((toPairs 0 v) ++ (toPairs 1 t)) where
 --
 
 data Tree a = Fork (Tree a) (Tree a) | Leaf a
-type FreqTree a = Tree (Int, a)
+type FreqTree a = Tree (a, Int)
 type FreqForest a = [FreqTree a]
 
 instance (Show a) => Show (Tree a) where
@@ -45,12 +44,8 @@ compareFreqTrees :: FreqTree a -> FreqTree a -> Ordering
 compareFreqTrees x y = compare (weight x) (weight y)
 
 weight :: FreqTree a -> Int
-weight (Leaf (n, _)) = n
+weight (Leaf (_, n)) = n
 weight (Fork x y) = (weight x) + (weight y)
-
-freqTableToForest :: Ord a => FreqTable a -> FreqForest a
-freqTableToForest = (L.sortBy compareFreqTrees) . (map pairToFreqItem) . M.toList where
-    pairToFreqItem (x, n) = (Leaf (n, x))
 
 freqForestToTree :: Ord a => FreqForest a -> FreqTree a
 freqForestToTree [] = error "the frequency forest is empty"
@@ -58,14 +53,14 @@ freqForestToTree [x] = x
 freqForestToTree (x: (y: s)) = freqForestToTree $ L.insertBy compareFreqTrees (Fork x y) s
 
 itemsToFreqTree :: Ord a => [a] -> [a] -> FreqTree a
-itemsToFreqTree v = freqForestToTree . freqTableToForest . itemsToFreqTable v
+itemsToFreqTree v = freqForestToTree . (L.sortBy compareFreqTrees) . (map Leaf) . M.toList . itemsToFreqTable v
 
 --
 -- Build Huffman code table from the tree
 --
 
 freqTreeToCodes :: Ord a => b -> (b -> b) -> (b -> b) -> FreqTree a -> M.Map a b
-freqTreeToCodes s _ _ (Leaf (_, x)) = M.singleton x s
+freqTreeToCodes s _ _ (Leaf (x, _)) = M.singleton x s
 freqTreeToCodes s f g (Fork u v) = M.union (subtreeToCodes f u) (subtreeToCodes g v) where
     subtreeToCodes h t = M.map h (freqTreeToCodes s f g t)
 
