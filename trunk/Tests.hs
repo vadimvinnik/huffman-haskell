682bcc92fa92ffe9663d5cd7b5a046716c0c6385
vadim.vinnik@gmail.com <vadim.vinnik@gmail.com@84efa2fc-7ed4-2164-f6bb-8c9606cd4d4f>
2014-08-13 21:16:50 +0000

    Add a test application

--- trunk/Tests.hs
+++ trunk/Tests.hs
@@ -2,11 +2,11 @@ import Huffman.Tree.Impl
 
 import qualified Data.Map as M
 
-alphabet1 = ""
-alphabet2 = [' '..'z']
-
-text1 = "too hot to hoot"
-text2 = "Haskell is a computer programming language. In particular, it is a polymorphically statically typed, lazy, purely functional language, quite different from most other programming languages. The language is named for Haskell Brooks Curry, whose work in mathematical logic serves as a foundation for functional languages. Haskell is based on the lambda calculus, hence the lambda we use as a logo."
+texts = [
+  "too hot to hoot",
+  "a quick brown fox jumps over the lazy dog",
+  "this is just a short test string",
+  "Haskell is a computer programming language. In particular, it is a polymorphically statically typed, lazy, purely functional language, quite different from most other programming languages. The language is named for Haskell Brooks Curry, whose work in mathematical logic serves as a foundation for functional languages. Haskell is based on the lambda calculus, hence the lambda we use as a logo."]
 
 treeToCodes :: HuffmanTree Char -> M.Map Char String
 treeToCodes = freqTreeToCodes "" ('0':) ('1':)
@@ -16,3 +16,13 @@ huffmanEncodeWith m = concat . (map (m M.!))
 
 huffmanDecodeString :: HuffmanTree Char -> String -> String
 huffmanDecodeString t = decode t . map (=='1')
+
+testHuffman :: String -> Bool
+testHuffman s = d == s where
+  d = huffmanDecodeString t e
+  e = huffmanEncodeWith m s
+  m = treeToCodes t
+  t = itemsToFreqTree "" s 
+
+main = do
+  putStrLn $ show $ map testHuffman texts
