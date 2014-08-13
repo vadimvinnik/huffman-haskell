03492209e80df8b4971ee371765079cad9444241
vadim.vinnik <vadim.vinnik@gmail.com>
2014-08-13 10:43:34 +0000

    A shorter frequency table builder

--- Huffman/Tree/Impl.hs
+++ Huffman/Tree/Impl.hs
@@ -21,24 +21,20 @@ import qualified Data.List as L
 type FreqTable a = M.Map a Int
 
 itemsToFreqTable :: Ord a => [a] -> [a] -> FreqTable a
-itemsToFreqTable v = foldl addToFreqTable (M.fromList $ map itemToPair v) where
-    itemToPair x = (x, 0)
-    addToFreqTable t x = updateFreqTable t (M.lookup x t) where
-        updateFreqTable t Nothing = M.insert x 1 t
-        updateFreqTable t (Just n) = M.insert x (n+1) t
-
+itemsToFreqTable v t = M.fromListWith (+) ((toPairs 0 v) ++ (toPairs 1 t)) where
+  toPairs x = L.map (flip (,) x)
 
 --
 -- Build Huffman tree from a frequency table
 --
 
-data FreqTree a = FreqNode (FreqTree a) (FreqTree a) | FreqItem Int a
+data FreqTree a = Fork (FreqTree a) (FreqTree a) | Leaf Int a
 type FreqForest a = [FreqTree a]
 
 instance Show a => Show (FreqTree a) where
   show = showIndent "" where
-    showIndent p (FreqItem n c) = (show c) ++ " " ++ (show n) ++ "\n"
-    showIndent p t@(FreqNode u v) =
+    showIndent p (Leaf n c) = (show c) ++ " " ++ (show n) ++ "\n"
+    showIndent p t@(Fork u v) =
       (show $ nodeFrequency t) ++
       "\n" ++ p ++ "|--" ++
       (showIndent (p ++ "|  ") u) ++
@@ -49,17 +45,17 @@ compareFreqTrees :: FreqTree a -> FreqTree a -> Ordering
 compareFreqTrees x y = compare (nodeFrequency x) (nodeFrequency y)
 
 nodeFrequency :: FreqTree a -> Int
-nodeFrequency (FreqItem n _) = n
-nodeFrequency (FreqNode x y) = (nodeFrequency x) + (nodeFrequency y)
+nodeFrequency (Leaf n _) = n
+nodeFrequency (Fork x y) = (nodeFrequency x) + (nodeFrequency y)
 
 freqTableToForest :: Ord a => FreqTable a -> FreqForest a
 freqTableToForest = (L.sortBy compareFreqTrees) . (map pairToFreqItem) . M.toList where
-    pairToFreqItem (x, n) = (FreqItem n x)
+    pairToFreqItem (x, n) = (Leaf n x)
 
 freqForestToTree :: Ord a => FreqForest a -> FreqTree a
 freqForestToTree [] = error "the frequency forest is empty"
 freqForestToTree [x] = x
-freqForestToTree (x: (y: s)) = freqForestToTree $ L.insertBy compareFreqTrees (FreqNode x y) s
+freqForestToTree (x: (y: s)) = freqForestToTree $ L.insertBy compareFreqTrees (Fork x y) s
 
 itemsToFreqTree :: Ord a => [a] -> [a] -> FreqTree a
 itemsToFreqTree v = freqForestToTree . freqTableToForest . itemsToFreqTable v
@@ -69,8 +65,8 @@ itemsToFreqTree v = freqForestToTree . freqTableToForest . itemsToFreqTable v
 --
 
 freqTreeToCodes :: Ord a => b -> (b -> b) -> (b -> b) -> FreqTree a -> M.Map a b
-freqTreeToCodes s _ _ (FreqItem _ x) = M.singleton x s
-freqTreeToCodes s f g (FreqNode u v) = M.union (subtreeToCodes f u) (subtreeToCodes g v) where
+freqTreeToCodes s _ _ (Leaf _ x) = M.singleton x s
+freqTreeToCodes s f g (Fork u v) = M.union (subtreeToCodes f u) (subtreeToCodes g v) where
     subtreeToCodes h t = M.map h (freqTreeToCodes s f g t)
 
 itemsToCodes :: Ord a => b -> (b -> b) -> (b -> b) -> [a] -> [a] -> M.Map a b
