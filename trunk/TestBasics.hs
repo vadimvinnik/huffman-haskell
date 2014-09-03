44b9f459de55d4d2a3730bcf479d306d69700c67
vadim.vinnik@gmail.com <vadim.vinnik@gmail.com@84efa2fc-7ed4-2164-f6bb-8c9606cd4d4f>
2014-09-03 02:13:54 +0000

    Refactor for orthogonal functionality and consistent naming

--- trunk/TestBasics.hs
+++ trunk/TestBasics.hs
@@ -1,8 +1,8 @@
 --
 -- A test application for Huffman encoding
 --
--- Only tests the worker functions representing binary
--- codes as strings of 0s and 1s for readability.
+-- Tests the worker functions representing
+-- binary codes by lists of Bool
 --
 -- (c) Vadim Vinnik, 2014
 --
@@ -11,19 +11,22 @@ import Huffman
 import Huffman.Show
 import qualified Data.Map as M
 
+toHistogram :: Ord a => [a] -> Histogram a
+toHistogram = M.fromListWith (+) . map (flip (,) 1)
+
 texts = [
   "too hot to hoot",
   "a quick brown fox jumps over the lazy dog",
   "this is just a short test string",
   "Haskell is a computer programming language. In particular, it is a polymorphically statically typed, lazy, purely functional language, quite different from most other programming languages. The language is named for Haskell Brooks Curry, whose work in mathematical logic serves as a foundation for functional languages. Haskell is based on the lambda calculus, hence the lambda we use as a logo."]
 
-decodeWithStrings :: HuffmanTree Char -> String -> String
+decodeWithStrings :: Tree Char -> String -> String
 decodeWithStrings t = decode t . map (=='1')
 
 data TestDetails = TestDetails {
   input   :: String,
-  tree    :: HuffmanTree Char,
-  codes   :: M.Map Char [Bool],
+  tree    :: Tree Char,
+  codes   :: CodeTable Char,
   encoded :: [Bool],
   decoded :: String
 } deriving (Show)
@@ -38,14 +41,15 @@ testHuffman s = TestDetails {
   } where
   d = decode t e
   e = encode m s
-  m = toCodeTable t
-  t = toTree "" s 
+  m = treeToCodeTable t
+  t = histogramToTree h
+  h = toHistogram s
 
 isPassed :: TestDetails -> Bool
 isPassed x = (input x) == (decoded x)
 
 main = do
-  mapM printTest texts where
+  mapM_ printTest texts where
   printTest s =
     let t = testHuffman s in
     if isPassed t
